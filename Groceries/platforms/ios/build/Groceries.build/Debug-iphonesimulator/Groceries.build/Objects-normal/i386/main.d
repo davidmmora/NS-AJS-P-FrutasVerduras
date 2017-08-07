23685736e68944d1c8780c89a1283851bbaebf0a
David <jekthor@gmail.com>
2017-08-07 18:19:43 -0500

    creacion de proyecto y primer display de imagen capitulo 2

---
+++ Groceries/platforms/ios/build/Groceries.build/Debug-iphonesimulator/Groceries.build/Objects-normal/i386/main.d
@@ -0,0 +1,10 @@
+dependencies: \
+  /Users/david/Projects/NativeScript/Angular/FrutasVerduras/NS-AJS-P-FrutasVerduras/Groceries/platforms/ios/Groceries/Groceries-Prefix.pch \
+  /Users/david/Projects/NativeScript/Angular/FrutasVerduras/NS-AJS-P-FrutasVerduras/Groceries/platforms/ios/internal/main.m \
+  /Users/david/Projects/NativeScript/Angular/FrutasVerduras/NS-AJS-P-FrutasVerduras/Groceries/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/david/Projects/NativeScript/Angular/FrutasVerduras/NS-AJS-P-FrutasVerduras/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/david/Projects/NativeScript/Angular/FrutasVerduras/NS-AJS-P-FrutasVerduras/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/david/Projects/NativeScript/Angular/FrutasVerduras/NS-AJS-P-FrutasVerduras/Groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/david/Projects/NativeScript/Angular/FrutasVerduras/NS-AJS-P-FrutasVerduras/Groceries/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/david/Projects/NativeScript/Angular/FrutasVerduras/NS-AJS-P-FrutasVerduras/Groceries/platforms/ios/internal/TNSDebugging.h \
+  /Users/david/Projects/NativeScript/Angular/FrutasVerduras/NS-AJS-P-FrutasVerduras/Groceries/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
