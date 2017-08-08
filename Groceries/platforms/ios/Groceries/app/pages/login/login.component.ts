8bc40742744e8df537d931d574f4444f466c6f67
David <jekthor@gmail.com>
2017-08-07 19:11:25 -0500

    3.5: Routing

---
+++ Groceries/platforms/ios/Groceries/app/pages/login/login.component.ts
@@ -0,0 +1,43 @@
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { User } from "../../shared/user/user";
+import { UserService } from "../../shared/user/user.service";
+
+@Component({
+  selector: "my-app",
+  providers: [UserService],
+  templateUrl: "pages/login/login.html",
+  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"],
+})
+export class AppComponent {
+  user: User;
+  isLoggingIn = true;
+
+  constructor(private router: Router, private userService: UserService) {
+  this.user = new User();
+  }
+  submit() {
+    if (this.isLoggingIn) {
+      this.login();
+    } else {
+      this.signUp();
+    }
+  }
+  login() {
+  this.userService.login(this.user)
+    .subscribe(
+      () => this.router.navigate(["/list"]),
+      (error) => alert("Unfortunately we could not find your account.")
+    );
+}
+  signUp() {
+  this.userService.register(this.user)
+    .subscribe(
+      () => {
+        alert("Your account was successfully created.");
+        this.toggleDisplay();
+      },
+      () => alert("Unfortunately we were unable to create your account.")
+    );
+}
+}
